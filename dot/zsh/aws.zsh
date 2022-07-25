33be97355a4f7c3578f75471a5951a28b7b779a3
Audrea Cook <51171867+AudTheCodeWitch@users.noreply.github.com>
2022-07-25 08:09:08 -0400

    Update alias

--- dot/zsh/aws.zsh
+++ dot/zsh/aws.zsh
@@ -1,1 +1,4 @@
-alias aws_sso="aws sso login --profile payments-kms-dev"
+# Before this alias works, you must set up a profile in ~/.aws/config
+# https://ben11kehoe.medium.com/you-only-need-to-call-aws-sso-login-once-for-all-your-profiles-41a334e1b37e
+
+alias aws_sso="aws sso login --profile login"
