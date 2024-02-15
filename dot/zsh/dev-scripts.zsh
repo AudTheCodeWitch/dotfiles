530fdc2809e28e465e87d874301c639c5e8aa302
Audrea Cook <audrea.cook@dutchie.com>
2024-02-15 09:31:38 -0500

    Add aliases

---
+++ dot/zsh/dev-scripts.zsh
@@ -0,0 +1,5 @@
+# Start BackOffice
+alias bo='git pull && nvm use && yarn knit'
+
+# Start POS
+alias pos='nvm use && yarn start'
--- dot/zsh/updbs.zsh
+++ dot/zsh/updbs.zsh
@@ -1,2 +1,17 @@
 # Updates the LLX GoldDb and IntegrationDB databases
-alias updbs="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/integrationdb.ps1 -rebuild && docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/golddb.ps1"
+alias updbs="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/integrationdb.ps1 && docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/golddb.ps1"
+
+# Rebuilds LLX IntegrationDB and updates GoldDB
+alias rbdbs="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/integrationdb.ps1 -rebuild && docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/golddb.ps1"
+
+# Updates LLX GoldDB
+alias upgold="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/golddb.ps1"
+
+# Rebuilds LLX GoldDB
+alias rebuildgold="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/golddb.ps1 -rebuild"
+
+# Updates LLX IntegrationDB
+alias upint="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/integrationdb.ps1"
+
+# Rebuilds LLX IntegrationDB
+alias rebuildint="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/integrationdb.ps1 -rebuild"
