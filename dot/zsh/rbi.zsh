f89fe2044f71580949884c64da9bfafd388e71a5
Audrea Cook <audrea.cook@dutchie.com>
2024-05-23 11:43:54 -0400

    Remove duplicate script

--- dot/zsh/rbi.zsh
+++
@@ -1,1 +0,0 @@
-alias updbs="docker compose -f docker-compose.yml -f docker-compose.vscode.override.yml -f docker-compose.m1.override.yml run webapi pwsh /app/webapi/Docker.Dev/integrationdb.ps1 -rebuild"
