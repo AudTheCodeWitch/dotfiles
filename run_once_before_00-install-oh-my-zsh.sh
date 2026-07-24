b7fbc83482a83340c804cf81af7308ac10b6fe43
Audrea Cook <audrea@codewitch.dev>
2026-07-24 15:01:54 -0600

    feat: add custom plugin installation to Oh My Zsh setup script

--- run_once_before_00-install-oh-my-zsh.sh
+++ run_once_before_00-install-oh-my-zsh.sh
@@ -5,3 +5,19 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "🚀 Installing Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
+
+# Define custom plugin dir
+ZSH_CUSTOM="${HOME}/.config/zsh"
+
+# Clone missing custom plugins
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-bat" ]; then
+  git clone https://github.com/fdellwing/zsh-bat "$ZSH_CUSTOM/plugins/zsh-bat"
+fi
